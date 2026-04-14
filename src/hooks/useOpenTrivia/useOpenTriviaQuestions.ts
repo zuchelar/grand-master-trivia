1edb07737df320da701306b7baa836e657ca224a
zuchelar <zuchelar@gmail.com>
2026-04-14 15:33:48 -0500

    feat: add trivia api (#1)
    * Adds React Query
    * Adds API call to fetch Open Trivia DB questions with category, difficulty, and amount filtration

---
+++ src/hooks/useOpenTrivia/useOpenTriviaQuestions.ts
@@ -0,0 +1,28 @@
+import { useQuery } from '@tanstack/react-query';
+import { type OpenTriviaCategory, type OpenTriviaDifficulty, type OpenTriviaQuestion } from '@/types/open-trivia';
+
+interface OpenTriviaQuestionsParams {
+	category_id: OpenTriviaCategory['id'];
+	difficulty: OpenTriviaDifficulty;
+	amount?: number;
+}
+
+interface OpenTriviaQuestionsResult {
+	response_code: number;
+	results: OpenTriviaQuestion[];
+}
+
+const useOpenTriviaQuestions = ({ category_id, difficulty, amount = 10 }: OpenTriviaQuestionsParams) => {
+	return useQuery<OpenTriviaQuestionsResult>({
+		queryKey: ['open-trivia-questions', category_id, difficulty, amount],
+		queryFn: async () => {
+			const response = await fetch(
+				`https://opentdb.com/api.php?amount=${amount}&category=${category_id}&difficulty=${difficulty}`,
+			);
+			const data = await response.json();
+			return data;
+		},
+	});
+};
+
+export default useOpenTriviaQuestions;
---
+++ src/types/open-trivia.ts
@@ -0,0 +1,58 @@
+const OPEN_TRIVIA_CATEGORIES = [
+	{ id: 9, name: 'General Knowledge' },
+	{ id: 10, name: 'Entertainment: Books' },
+	{ id: 11, name: 'Entertainment: Film' },
+	{ id: 12, name: 'Entertainment: Music' },
+	{ id: 13, name: 'Entertainment: Musicals & Theatres' },
+	{ id: 14, name: 'Entertainment: Television' },
+	{ id: 15, name: 'Entertainment: Video Games' },
+	{ id: 16, name: 'Entertainment: Board Games' },
+	{ id: 17, name: 'Science & Nature' },
+	{ id: 18, name: 'Science: Computers' },
+	{ id: 19, name: 'Science: Mathematics' },
+	{ id: 20, name: 'Mythology' },
+	{ id: 21, name: 'Sports' },
+	{ id: 22, name: 'Geography' },
+	{ id: 23, name: 'History' },
+	{ id: 24, name: 'Politics' },
+	{ id: 25, name: 'Art' },
+	{ id: 26, name: 'Celebrities' },
+	{ id: 27, name: 'Animals' },
+	{ id: 28, name: 'Vehicles' },
+	{ id: 29, name: 'Entertainment: Comics' },
+	{ id: 30, name: 'Science: Gadgets' },
+	{ id: 31, name: 'Entertainment: Japanese Anime & Manga' },
+	{ id: 32, name: 'Entertainment: Cartoon & Animations' },
+];
+
+interface OpenTriviaCategory {
+	id: (typeof OPEN_TRIVIA_CATEGORIES)[number]['id'];
+	name: (typeof OPEN_TRIVIA_CATEGORIES)[number]['name'];
+}
+
+const OPEN_TRIVIA_DIFFICULTIES = {
+	EASY: 'easy',
+	MEDIUM: 'medium',
+	HARD: 'hard',
+} as const;
+
+type OpenTriviaDifficulty = (typeof OPEN_TRIVIA_DIFFICULTIES)[keyof typeof OPEN_TRIVIA_DIFFICULTIES];
+
+const OPEN_TRIVIA_QUESTION_TYPES = {
+	MULTIPLE: 'multiple',
+	BOOLEAN: 'boolean',
+} as const;
+
+type OpenTriviaQuestionType = (typeof OPEN_TRIVIA_QUESTION_TYPES)[keyof typeof OPEN_TRIVIA_QUESTION_TYPES];
+
+interface OpenTriviaQuestion {
+	type: OpenTriviaQuestionType;
+	difficulty: OpenTriviaDifficulty;
+	category: (typeof OPEN_TRIVIA_CATEGORIES)[number]['name'];
+	question: string;
+	correct_answer: string;
+	incorrect_answers: string[];
+}
+
+export type { OpenTriviaCategory, OpenTriviaDifficulty, OpenTriviaQuestion };
+export { OPEN_TRIVIA_CATEGORIES, OPEN_TRIVIA_DIFFICULTIES, OPEN_TRIVIA_QUESTION_TYPES };
