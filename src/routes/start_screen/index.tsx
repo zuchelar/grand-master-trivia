b0b4fe1fda5b7c2141b4efa949e65e1bdb911aed
zuchelar <zuchelar@gmail.com>
2026-04-14 16:45:24 -0500

    feat: add difficulty card

--- src/routes/start_screen/index.tsx
+++ src/routes/start_screen/index.tsx
@@ -1,15 +1,18 @@
 import { useState } from 'react';
-import type { OpenTriviaCategory } from '@/types/open-trivia';
+import type { OpenTriviaCategory, OpenTriviaDifficulty } from '@/types/open-trivia';
 import CategoryCard from './_components/CategoryCard';
+import DifficultyCard from './_components/DifficultyCard';
 import TopCard from './_components/TopCard';
 
 const StartScreen = () => {
 	const [categoryId, setCategoryId] = useState<OpenTriviaCategory['id'] | null>(null);
+	const [difficulty, setDifficulty] = useState<OpenTriviaDifficulty | null>(null);
 
 	return (
 		<div className='mx-auto flex w-full max-w-2xl flex-col gap-4'>
 			<TopCard />
 			<CategoryCard value={categoryId} onValueChange={setCategoryId} />
+			<DifficultyCard value={difficulty} onValueChange={setDifficulty} />
 		</div>
 	);
 };
